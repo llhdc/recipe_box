0cd4ea6e0299770ff53444020bcb600af098cdb8
Leonard <leonard.horne@gmail.com>
2017-09-05 20:59:13 -0400

    Add seed data using Faker gem.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+25.times do
+  recipe = Recipe.create!(name: Faker::Food.dish, makes: rand(1..10))
+
+
+  rand(1..5).times do
+    recipe.ingredients.create(element: Faker::Food.ingredient, quantity: rand(1..5), metric_measurement: Faker::Food.metric_measurement)
+  end
+
+  rand(1..5).times do
+    recipe.steps.create(description: Faker::Coffee.notes)
+  end
+end
